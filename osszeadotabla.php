706926884ee5bb2fccd216d9ceb4d64ef441a206
Bóka Tamás <tomi500offical@gmail.com>
2025-09-21 16:57:18 +0200

    done

--- osszeadotabla.php
+++ osszeadotabla.php
@@ -1,12 +1,36 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Title</title>
-</head>
-<body>
-    <?php
+<style>
+    table, td, th {
+        border: 1px solid #999;
+        border-collapse: "collapse";
+        padding: 4px;
+        text-align: center;
+    }
+</style>
 
-    ?>
-</body>
-</html>
+<?php
+    $size = 10;
+?>
+<hr>
+
+<table>
+    <tr>
+        <th>&plus;</th>
+        <?php for ($i = 1; $i <= $size; $i++) : ?>
+            <th>
+                <?= $i ?>
+            </th>
+        <?php endfor; ?>
+    </tr>
+    <?php for ($row = 1; $row <= $size; $row++) : ?>
+        <tr>
+            <th>
+                <?= $row ?>
+            </th>
+            <?php for ($col = 1; $col <= $size; $col++) : ?>
+                <td>
+                    <?= $row + $col; ?>
+                </td>
+            <?php endfor; ?>
+        </tr>
+    <?php endfor; ?>
+</table>
